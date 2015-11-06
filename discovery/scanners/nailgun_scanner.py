160e2a1353781a865046f7c44c821c27873ccca4
Evgeniy L <eli@mirantis.com>
2015-11-06 22:08:14 +0300

    Implement nailgun scanner

--- discovery/scanners/nailgun_scanner.py
+++ discovery/scanners/nailgun_scanner.py
@@ -14,11 +14,18 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import 
+from oslo_config import cfg
 from oslo_log import log
+from oslo_service import periodic_task
+from oslo_service import service
+from oslo_service import threadgroup
+from keystoneclient.v2_0 import client
+
 from discovery.scanners.base import BaseScanner
 
 
-LOG = log.getLogger()
+LOG = log.getLogger(__name__)
 
 
 class NailgunScanner(BaseScanner):
@@ -26,8 +33,82 @@ class NailgunScanner(BaseScanner):
     name = 'nailgun_scanner'
     version = '1.0.0.dev'
 
-    def __init__(self, conf):
-        pass
+    cli_opts = [
+        cfg.StrOpt(
+            'nailgun_nodes_endpoint',
+            default='http://10.20.0.2:8000/api/v1/nodes/',
+            help='Nailgun api endpoint'),
+        # TODO in production these parameters should be
+        # read from config, not to expose them in bash
+        # history
+        cfg.StrOpt(
+            'nailgun_password',
+            default='admin',
+            help='Nailgun password'),
+        cfg.StrOpt(
+            'nailgun_user',
+            default='admin',
+            help='Nailgun user'),
+        cfg.StrOpt(
+            'nailgun_keystone_endpoint',
+            default='http://0.0.0.0:8000/keystone/v2.0',
+            help='Nailgun api endpoint'),
+        cfg.StrOpt(
+            'nailgun_keystone_tenant',
+            default='admin',
+            help='Keystone tenant for nailgun'),
+        cfg.IntOpt(
+            'nailgun_interval',
+            default=5,
+            help='Scanning interval')]
 
     def run(self):
-        LOG.info('Starting scanner %s %s', name, version)
+        LOG.info('Starting scanner %s %s', self.name, self.version)
+        keystone = client.Client(
+             username=self.config.nailgun_user,
+             password=self.config.nailgun_password,
+             tenant_name=self.config.nailgun_keystone_tenant,
+             auth_url=self.config.nailgun_keystone_endpoint)
+        keystone.authenticate()
+
+        class PeriodicTask(periodic_task.PeriodicTasks):
+            @periodic_task.periodic_task(
+                run_immediately=True,
+                spacing=self.config.nailgun_interval)
+            def run(_self, *args, **kwargs):
+                self.on_tick(keystone)
+
+        task = PeriodicTask(self.config)
+        
+        tg = threadgroup.ThreadGroup()
+        tg.add_dynamic_timer(task.run, context=None)
+
+        srv = service.Service()
+        srv.tg.add_dynamic_timer(task.run, context=None)
+
+        service_launcher = service.ServiceLauncher(self.config)
+        service_launcher.launch_service(srv)
+        service_launcher.wait()
+
+    def on_tick(self, keystone):
+        try:
+            LOG.exception('Getting nodes from Nailgun %s', self.config.nailgun_nodes_endpoint)
+            nodes = keystone.get(self.config.nailgun_nodes_endpoint)[1]
+
+            for node in nodes:
+                try:
+                    node_id = self._match_node(node)
+
+                    if node_id is None:
+                        self.create_node(node)
+                    else:
+                        self.update_node(node_id, node)
+                except Exception as exc:
+                    LOG.exception(exc)
+                    LOG.error("Failed to update the node %s", node)
+
+        except Exception as exc:
+            LOG.exception(exc)
+
+    def _get_matching_data(self, data):
+        return {'source_id': 'nailgun_{0}'.format(data['id'])}
